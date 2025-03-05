2da3e77227df5b0d477d90acc7e9699924365228
Fis <evx9000@gmail.com>
2025-03-05 22:07:22 +0300

    iuiuiuiui

--- globals.py
+++ globals.py
@@ -1,4 +1,6 @@
 class GlobalConfig:
+    # PROD
+
     # Бот в группе обратной связи
     feedback_tg_bot = "logistic_feedback"
     feedback_tg_bot_username = "logisticFeedback_bot"
@@ -10,10 +12,29 @@ class GlobalConfig:
     edit_feedback_bot_username = "logisticEditFeedback_bot"
     edit_feedback_tg_bot_token = "8082382435:AAGbAfLFcFEYl99jV_0nvhzZFq5YfKK27_0"
     edit_feedback_tg_bot_group_id = -1002316643090 # Прод
-    # edit_feedback_tg_bot_group_id = -1002491352171 # Локалка
-    # Бот оплаты
 
+    # Бот оплаты
     pay_tg_bot = "logistic_pay"
     pay_bot_username = "logisticPay_bot"
     pay_tg_bot_token = "7615215575:AAFfp8xoJBhtVfmXJPi_5dnMJGrB6QrCFw4"
 
+    ###
+
+    # LOCAL
+
+    # # Бот оплаты LOCAL
+    # pay_tg_bot = "logistic_pay_local_bot"
+    # pay_bot_username = "logisticPayLocal_bot"
+    # pay_tg_bot_token = "8067888336:AAEgr5NVPwd4ldT50nTu1ESsh36YTPGQeN0"
+
+    # # Бот в группе обратной связи LOCAL
+    # feedback_tg_bot = "logistic_feedback_local_bot"
+    # feedback_tg_bot_username = "logisticFeedbackLocal_bot"
+    # feedback_tg_bot_token = "7779108146:AAEqL0ND8lZ9lQAU_K9WmBIjHUTyZhug2w4"
+    # # feedback_tg_bot_group_id = -1002449070638
+
+    # # Бот в группе редактирования команд бота обратной связи LOCAL
+    # edit_feedback_tg_bot = "logistic_edit_feedback_local_bot"
+    # edit_feedback_bot_username = "logisticEditFeedbackLocal_bot"
+    # edit_feedback_tg_bot_token = "7649033395:AAES6_01JC0cK93wPaXIlyVTutZcYkKu8II"
+    # edit_feedback_tg_bot_group_id = -1002491352171 # Локалка
