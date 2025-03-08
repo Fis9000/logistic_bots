45765af4eae9aa3fa99719da106c7b1eff0cb7c3
Fis <evx9000@gmail.com>
2025-03-08 15:15:55 +0300

    red

--- tg_bots/actions/send_message.py
+++ tg_bots/actions/send_message.py
@@ -1,7 +1,7 @@
 from telegram import Bot, Update
 from telegram.error import TelegramError
 
-# Функция отправки сообщений
+# Функция отправки сообщений в группу
 async def send_message_to_group(_tg_token, _group_id, _text):
     bot = Bot(token=_tg_token)
     try:
@@ -9,3 +9,4 @@ async def send_message_to_group(_tg_token, _group_id, _text):
         await bot.send_message(chat_id=_group_id, text=_text)
     except TelegramError as e:
         print(f"Ошибка при отправке сообщения: {e}")
+
--- tg_bots/feedback_bot_grp/start.py
+++ tg_bots/feedback_bot_grp/start.py
@@ -13,10 +13,21 @@ async def incoming_messages():
     bot = Bot(token=TELEGRAM_TOKEN)
     last_update_id = None
 
+    # Очищаем очередь обновлений при старте
+    updates = await bot.get_updates()
+    if updates:
+        last_update_id = updates[-1].update_id + 1
+        await bot.get_updates(offset=last_update_id)  # Очищаем очередь
+
     while True:
         try:
             # Получаем обновления с offset, чтобы избежать повторений
             updates = await bot.get_updates(offset=last_update_id)
+
+            # Если обновлений нет, ждем и продолжаем
+            if not updates:
+                time.sleep(1)
+                continue
 
             # Загружаем ключевые слова и ответы из JSON-файла
             key_responses = await load_key_responses()
@@ -25,6 +36,7 @@ async def incoming_messages():
                 if update.message:
                     user_message = update.message.text
                     user_name = update.message.from_user.first_name
+                    user_message_id = update.message.id
 
                     # Логи
                     print(f"{user_name} написал - '{user_message}'")
@@ -36,13 +48,12 @@ async def incoming_messages():
                         for keyword, response in key_responses.items():
                             if keyword == message_text:
                                 await send_message_to_group(TELEGRAM_TOKEN, update.message.chat_id, response)
-                                # await send_message_to_group(TELEGRAM_TOKEN, GROUP_ID, response)
                     # Обновляем last_update_id для получения только новых сообщений
                     last_update_id = update.update_id + 1
 
             time.sleep(1)  # задержка
         except TelegramError as e:
-            print(f"Ошибка при получении обновлений: {e}")
-
+            print(f"Ошибка при получении обновлений: {e}")     
+                   
 async def feedback_bot_grp_start_bot():
     await incoming_messages()
